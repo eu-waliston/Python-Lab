3c63d51ce7558a20bd7bffaa0e7a0ff3a646587c
Waliston Euripedes <goneisgone@hotmail.com>
2022-10-06 22:56:07 -0300

    atualizações

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -3,4 +3,5 @@ from bs4 import BeautifulSoup
 
 html = urlopen('https://www.ufsm.br')
 bs = BeautifulSoup(html.read(), 'html.parser')
-print(bs.body.p) #Responsável por ´FILTRAR´ a busca dentro dos sites
+print(bs.body.p)  # Responsável por ´FILTRAR´ a busca dentro dos sites
+
