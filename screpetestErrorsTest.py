20c6874c2150c2e2355290cccf1fdeecdef43c3d
Waliston Euripedes <goneisgone@hotmail.com>
2022-10-06 23:07:33 -0300

    melhoria na busca e na captação de erros

--- screpetestErrorsTest.py
+++ screpetestErrorsTest.py
@@ -1,12 +1,19 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
-from urllib.error import URLError
-
-try:
-    html = urlopen('https://pythonscrapingthisurldoesnotexist.com')
-except HTTPError as e:
-    print(e)
-except URLError as e:
-    print('The server could not be found!')
+from bs4 import BeautifulSoup
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        title = bs.body.h1
+    except AttributeError as e:
+        return None
+    return title
+title = getTitle('http://www.pythonscraping.com/pages/page1.html')
+if title == None:
+    print('Title could not be found')
 else:
-    print('It Worked!')
+    print(title)
