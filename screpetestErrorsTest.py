3c5bee6375d52b80890c51c74a3ea8812f245fc6
Waliston Euripedes <goneisgone@hotmail.com>
2022-10-06 23:11:27 -0300

    refatoração e fim do capitulo 1

--- screpetestErrorsTest.py
+++ screpetestErrorsTest.py
@@ -1,6 +1,8 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
+
+
 def getTitle(url):
     try:
         html = urlopen(url)
@@ -12,8 +14,10 @@ def getTitle(url):
     except AttributeError as e:
         return None
     return title
+
+
 title = getTitle('http://www.pythonscraping.com/pages/page1.html')
-if title == None:
+if title is None:
     print('Title could not be found')
 else:
     print(title)
